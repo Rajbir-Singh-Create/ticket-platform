2c2f39489c5bc1e33ebe2a47dec5a8d844482075
Rajbir-Singh-Create <rajbir.singh.create@gmail.com>
2025-05-14 19:50:44 +0200

    First commit

---
+++ src/main/java/org/rajcreate/java/spring/ticketplatform/TicketplatformApplication.java
@@ -0,0 +1,13 @@
+package org.rajcreate.java.spring.ticketplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketplatformApplication.class, args);
+	}
+
+}
